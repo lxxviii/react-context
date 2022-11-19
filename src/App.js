34508bb5f44cb85991286a1d2400dfaacba30e53
O.Bora TUNÇ <lxxviii@outlook.com>
2022-11-19 16:21:03 +0300

    Fetch Get, Post Service ayarları default hale getirildi.

--- src/App.js
+++ src/App.js
@@ -1,83 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { PostService, UserService } from './services'
+//TODO çoklu isim tekli dinamik hale getirildi
+//import { getPosts, getPostDetail, newPosts } from './services';
 
 function App() {
- 
-  // TODO : 1.Seviye Tamamlandı, Tüm Componentler Bir Arada
-  const [users, setUsers] = useState(false)
-  const [name, setName] = useState('BORA')
-  const [avatar, setAvatar] = useState(false)
-
-  const addPost = (data) => {
-
-    const headers = new Headers();
-    //headers.append('Content-type', 'application/json')
-    headers.append('Authorization', 'Bearer bora1234abc')
-
-    const formData = new FormData()
-    formData.append('userId', data.userId)
-    formData.append('title', data.title)
-    formData.append('body', data.body)
-
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      //body: JSON.stringify(data),
-      body: formData,
-      headers, //headers:headers olarak yazılabilir
-    })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.log(error))
-  }
 
   useEffect(() => {
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(
-        response => {
-          if (response.ok && response.status === 200) {
-            return response.json();
-          }
-        })
-      .then(data => { setUsers(data) })
-      .catch(error => console.log(error))
+    PostService.getPosts().then((response) => console.log(response))
+    PostService.getPostDetail(2).then((response) => console.log(response))
+    PostService.newPost({
+      userId: 3,
+      title: 'test',
+      body: 'Bu Bir TEST bodydir.'
+    }).then((response) => console.log(response))
 
-    addPost({
-      userId: 1,
-      title: 'Örnek Post',
-      body: 'Post İçeriği'
-    });
+    UserService.getUsers()
 
   }, [])
 
-
-  const submitHandle = (e) => {
-    e.preventDefault()
-    const formData = new FormData()
-    formData.append('name', name)
-    formData.append('avatar', avatar)
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: formData
-    })
-  }
-
   return (
     <>
-      <form onSubmit={submitHandle}>
-        <input type='text' name='name' value={name} onChange={e => setName(e.target.value)} /> <br />
-        <input type='file' name='avatar' onChange={e => setAvatar(e.target.files[0])} /> <br />
-        <button type='submit' disabled={!name || !avatar}> Kaydet </button>
-  </form>
-      <h2> USER LIST </h2>
-      <ul>
-        {users && users.map(user => (
-          <li key={user.id}>
-            {user.name}
-          </li>
-        ))}
-      </ul>
+      APP
     </>
-
   );
 }
-export default App;
+export default App
---
+++ src/services/index.js
@@ -0,0 +1,12 @@
+export * as PostService from './post';
+export * as UserService from './user';
+
+//TODO ancak yine de tek bir merkezden yönlendirmek istenilirse yukarıdaki gibi yapılır.
+//export { getPosts, newPost, getPostDetail } from "./post";
+
+//TODO import ederken export ederek kullanılabilir, import kısmına export yazılır
+/*export {
+    getPosts,
+    newPost,
+    getPostDetail
+}*/
